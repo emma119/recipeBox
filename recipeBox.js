7fffcf22477cb390ff72b845a441cf5ad241507d
Emma <paparuz.manuela@gmail.com>
2021-01-31 12:26:39 +0200

    displaying the first recipe on the page refresh/ inserting recipes in Modal and saving them/ displaying the ingredients and directions as a list

--- recipeBox.js
+++ recipeBox.js
@@ -3,7 +3,7 @@ var app = new Vue({
     el: '#app',
     data(){
         return {
-           
+
             recipes: [
                 { id: 0, title: 'Artichoke Pasta', description: '2 tablespoons butter, 2 cloves garlic, minced, 1 cup heavy cream, 3/4 teaspoon salt, 1 teaspoon fresh-ground black pepper, 2 1/2 cups canned, drained artichoke hearts (two 14-ounce cans), rinsed and cut into halves or quarters, 3/4 pound fusilli, 1/2 cup grated Parmesan cheese, 2 tablespoons chopped chives, scallion tops, or parsley', directions: 'In a medium saucepan, melt the butter over moderately low heat. Add the garlic and cook for 30 seconds. Stir in the cream, salt, pepper, and artichoke hearts. Cook until just heated through, about 3 minutes. In a large pot of boiling, salted water, cook the fusilli until just done, about 13 minutes. Drain the pasta and toss with the cream sauce, Parmesan, and chives.'},
                 { id: 1, title: 'Garlic Chicken', description: '3 tablespoons, butter 1, teaspoon seasoning salt, 1 teaspoon onion powder, 4 skinless boneless chicken breast halves, 2 teaspoons garlic powder', directions: 'Melt butter in a large skillet over medium high heat. Add chicken and sprinkle with garlic powder, seasoning salt and onion powder. Saute about 10 to 15 minutes on each side, or until chicken is cooked through and juices run clear.'},
@@ -11,32 +11,45 @@ var app = new Vue({
                 {id: 3, title:'Lime Chicken Tacos', description: '1 1/2 pounds skinless, boneless chicken breast meat - cubed, 1/8 cup red wine vinegar, 1/2 lime, juiced, 1 teaspoon white sugar, 1/2 teaspoon salt, 1/2 teaspoon ground black pepper, 2 green onions, chopped, 2 cloves garlic, minced, 1 teaspoon dried oregano, 10 (6 inch) corn tortillas, 1 tomato, diced, 1/4 cup shredded lettuce, 1/4 cup shredded Monterey Jack cheese, 1/4 cup salsa', directions: 'Saute chicken in a medium saucepan over medium high heat for about 20 minutes. Add vinegar, lime juice, sugar, salt, pepper, green onion, garlic and oregano. Simmer for an extra 10 minutes. Heat an iron skillet over medium heat. Place a tortilla in the pan, warm, and turn over to heat the other side. Repeat with remaining tortillas. Serve lime chicken mixture in warm tortillas topped with tomato, lettuce, cheese and salsa.'},
                 {id: 4, title:'Artichoke Dip', description: '1 8oz package soft cream cheese, 4oz mayonnaise, 4oz sour cream, 1/4 Cup Fresh Grated Parmesan Cheese, 1/4 Cup Fresh Grated Romano Cheese, 2 eggs, 3/4 Cup dairy sour cream, 1 16oz can artichoke hearts, 1/4 Cup fresh chopped chives, 1 tbs fresh minced garlic', directions: 'Soften the cream cheese before mixing. Rinse well, then dice the artichokes into small ½” size pieces. Into a mixing bowl place the softened cream cheese. Mix in the mayonnaise, sour cream, the Parmesan and Romano cheese, artichokes and garlic. When the mixture is fairly well blended, spoon into a 9” round glass pie dish. Set Oven to Bake at 350 degrees. Place un-covered dish into oven for 20 – 25 minutes until the edges appear slightly golden and mixture is bubbling at the edge. Remove and sprinkle chopped chives on top and let cool about 5 minutes before serving. Enjoy!'}
             ],
-            currentRecipe: '',
-            currentId: 5,
+            currentRecipeTitle: '',
+            currentRecipeDescription: '',
+            currentRecipeDirections: '',
             clickedRecipeDescription: '',
             clickedTitleDescription: '',
-            clickedDirectionsDescription: ''
- 
+            clickedDirectionsDescription: '',
+            currentId:5 
+           
+
         }
-    },
+    }, 
+    created:function(){
+                this.recipesDescription(this.recipes[0])
+            },
+    
+    
     methods: {
         saveRecipe(){
             this.recipes.push({
                 id: this.currentId++,
-                title: this.currentRecipe
+                title: this.currentRecipeTitle,
+                description: this.currentRecipeDescription,
+                directions: this.currentRecipeDirections
             })
-            this.currentRecipe = ''
+            this.currentRecipeTitle = '',
+            this.currentRecipeDescription= '',
+            this.currentRecipeDirections= ''
+        },
+  
+        
+        recipesDescription:function(reteta){
+            this.clickedTitleDescription=reteta.title,
+                this.clickedRecipeDescription = reteta.description,
+                this.clickedDirectionsDescription=reteta.directions
+
         },
 
-        recipesDescription(title, description, directions ){
 
-            this.clickedTitleDescription=title,
-            this.clickedRecipeDescription = description,
-          this.clickedDirectionsDescription=directions
-                
-        }
-        
-        
+       
     }
 })
 
@@ -49,19 +62,20 @@ var btn = document.getElementById("myBtn");
 
 var span = document.getElementsByClassName("close")[0];
 
- 
+var span = document.getElementsByClassName("closeSave")[0];
+
 btn.onclick = function() {
-  modal.style.display = "block";
+    modal.style.display = "block";
 }
 
 
 span.onclick = function() {
-  modal.style.display = "none";
+    modal.style.display = "none";
 }
 
 
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
